d18f8a3ea52d64f308a5b86a16d4b8755f842a64
Yashika118 <kumariyashika78@gmail.com>
2025-03-25 00:40:57 +0530

    problem in supabase connection

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import supabase from '../db/supabase/supabaseClient.js';
 
 dotenv.config();
 const app=express();
 const port=process.env.PORT || 5000;
 
 
+app.get("api/test-supabase",async(req,res)=>{
+    try {
+
+        const {data,error}=await supabase.from('test').select('*');
+        if(error){
+            console.error('Supabase error:', error.message)
+            return res.status(500).json({ error: 'Supabase connection failed.' })
+        }
+        res.status(200).json({ message: 'Supabase connected successfully!', data });
+
+    } catch (error) {
+        console.error('Server error:', err)
+        res.status(500).json({ error: 'Server error while testing Supabase.' })
+    }
+})
 
 app.listen(port,()=>{
 
     console.log(`Server is running on port ${port} successfully`);
+    
 })
